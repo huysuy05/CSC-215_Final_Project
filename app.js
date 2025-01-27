9cf02a1fe979189bbcf0147fc371170cf1cf0d2a
Huy Nguyen <huynguyen23@augustana.edu>
2025-01-26 19:49:31 -0600

    added new html files

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
 
+// Import the database and the url to the cluster
+const mongoose = require("mongoose");
+const dbUrl = "mongodb+srv://huysuy05:ditcumay@cluster05.gbowi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster05y";
+
+
 // app.js
 
 // Import necessary modules
@@ -9,15 +14,21 @@ const path = require('path');
 const app = express();
 
 // Set the port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5500;
 
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Basic route
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.sendFile();
 });
+
+app.get("/about", (req, res) => {
+    res.sendFile("")
+})
+
+
 
 // Start the server
 app.listen(PORT, () => {
